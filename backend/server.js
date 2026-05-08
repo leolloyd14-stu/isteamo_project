08afe264e9887265b94ac497759853165081e03c
leolloyd14-stu <adjeimensahlloyd@gmail.com>
2026-05-08 23:17:29 +0000

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: function(origin, callback){
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null, true);
-        }else{
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: "https://leolloyd14-stu.github.io",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"]
 }));
