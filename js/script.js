59a1b4565ee4d6e85632734e34c65a0609851791
leolloyd14-stu <adjeimensahlloyd@gmail.com>
2026-05-16 11:15:45 +0000

    S7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 const API_URL = "https://isteamo-project.onrender.com/chat";
+
+function showMoodMessage(mood) {
+  const messageBox = document.getElementById("moodMessage");
+
+  if (mood === "calm") {
+    messageBox.textContent = "Great! Your mind feels peaceful today 🌿";
+  } 
+  else if (mood === "okay") {
+    messageBox.textContent = "You're doing okay. Keep taking small calm steps 🌱";
+  } 
+  else if (mood === "stressed") {
+    messageBox.textContent = "Take a deep breath. Try a short calming activity 💚";
+  }
+}
